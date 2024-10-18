cf917ebaf6258b1e609d063e4d083823e9570a93
Anand Patidar <patidaranand9752@gmail.com>
2024-10-18 16:26:27 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import { useState } from 'react';
 import RefrshHandler from './RefrshHandler';
 
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <div className="App">
+      
       <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
         <Route path='/' element={<Navigate to="/login" />} />
